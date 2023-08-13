cfa5e71739b98dc0ffe4f6cb8fc26ba4f6b499c5
Ludovic Roland <ludovic.c.roland@gmail.com>
2023-08-13 23:09:40 +0200

    feat(disconnect): Add the possibility to disconnect from the app

--- app/src/main/java/com/aura/ui/home/HomeActivity.kt
+++ app/src/main/java/com/aura/ui/home/HomeActivity.kt
@@ -1,8 +1,13 @@
 package com.aura.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import com.aura.R
 import com.aura.databinding.ActivityHomeBinding
+import com.aura.ui.login.LoginActivity
 
 class HomeActivity : AppCompatActivity()
 {
@@ -19,6 +24,27 @@ class HomeActivity : AppCompatActivity()
     val balance = binding.balance
 
     balance.text = "2654,54€"
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu?): Boolean
+  {
+    menuInflater.inflate(R.menu.home_menu, menu)
+    return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean
+  {
+    return when (item.itemId)
+    {
+      R.id.disconnect ->
+      {
+        startActivity(Intent(this@HomeActivity, LoginActivity::class.java))
+        finish()
+        true
+      }
+
+      else            -> super.onOptionsItemSelected(item)
+    }
   }
 
 }
